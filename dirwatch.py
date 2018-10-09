eaabacf6365f3b397f547eaa9415f37cbd5e90b7
mdshepard <mdshepard8@gmail.com>
2018-10-09 15:11:38 -0400

    Yep, further along. Fixed the nasty argparse error

--- dirwatch.py
+++ dirwatch.py
@@ -7,10 +7,16 @@ import sys
 
 # my ol' sloppy logger
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
-handle_file = logging.FileHandler('test.log')
-formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(message)s')
-logger.addHandler(handle_file)
+
+
+def setup_logger():
+    fmt = '%(asctime)s:%(levelname)s:%(message)s'
+    # formatter = logging.Formatter(fmt)
+    logging.basicConfig(format=fmt, level=logging.DEBUG)
+    file_handler = logging.FileHandler('dirwatch.log')
+    logger.addHandler(file_handler)
+
+
 exit_flag = False
 
 file_dict = {}
@@ -29,27 +35,30 @@ def handle_signal(sig, stack):
 def find_magic(file, directory):
     """checks for magic word"""
     magic_word = 'wizard'
-    with open(directory) as f:
-        for i, line in enumerate(f.readlines()):
-            if magic_word in line and i not in file_dict[file]:
+    with open(os.path.join(directory, file)) as f:
+        for i, line in enumerate(f):
+            if i <= file_dict[file]:
+                continue
+            if magic_word in line:
                 logger.info('{} found at line {} in {}'
                             .format(magic_word, i + 1, file))
-                file_dict[file].append(i)
+        file_dict[file] = i
 
 
-def watch_directory(dir):
-    path = ""
-    directory = path.join([os.path.relpath(os.getcwd), "/", dir])
-    print "setting the path to {1} where we'll search for files".format(
-        directory)
+def watch_directory(dir, ext):
+    # path = ""
+    directory = os.path.abspath(dir)
+    # logger.info("setting the path to {0} where we'll search for files".format(
+    #     directory)
+    # )
 
-    watched_files = [f for f in os.listdir(directory) if ".txt" in f]
+    watched_files = [f for f in os.listdir(directory) if f.endswith(ext)]
 
     if len(watched_files) > len(file_dict):
         for file in watched_files:
             if file not in file_dict:
                 logger.info(' {} found in {}'.format(file, dir))
-                file_dict[file] = []
+                file_dict[file] = 0
     elif len(watched_files) < len(file_dict):
         for file in file_dict:
             if file not in watched_files:
@@ -70,7 +79,7 @@ def create_parser():
         )
 
     parser.add_argument(
-        '-i', '--interval', type=int, default=4,
+        '-i', '--interval', type=int, default=2,
         help="How often the directory is to be watched in seconds."
     )
 
@@ -83,34 +92,41 @@ def create_parser():
 
 def main(args):
 
+    setup_logger()
+
     parser = create_parser()
 
     start_time = time.time()
 
-    args = parser.parse_args(args)
+    arg_namespace = parser.parse_args(args)
 
-    logger.info('Searching the following directory: {}'.format(args.dir))
-    print "starting program..."
+    logger.info('Searching the following directory: {}'.format(
+        arg_namespace.dir)
+    )
+    logger.info("starting {}...".format(__name__))
 
     signal.signal(signal.SIGINT, handle_signal)
     signal.signal(signal.SIGTERM, handle_signal)
 
-    while not exit_flag:
-        time.sleep(args.interval)
-        logger.info(
-            "Starting main while loop in {} seconds".format(args.interval)
+    logger.info(
+        "Polling of directory every {} seconds".format(
+            arg_namespace.interval)
         )
+    while not exit_flag:
+        time.sleep(arg_namespace.interval)
+
         try:
-            watch_directory(dir)
+            watch_directory(arg_namespace.dir, arg_namespace.ext)
         except IOError:
             logger.exception('no directory? aint nobody got time for that!')
-            print "Error,\ can't find directory"
-            time.sleep(args.interval)
-
+            logger.error("Error, not found: {}".format(
+                arg_namespace.dir)
+            )
         except Exception:
             logger.exception('unknown exception')
-            time.sleep(args.interval)
-    logger.info('time running: {} seconds'.format(time.time() - start_time))
+    logger.info('polling completed, uptime: {0:.1f} seconds'.format(
+        time.time() - start_time)
+    )
 
 
 if __name__ == '__main__':
